2d2324a0e7f68286ba07ca09453f1b5796f8b2e2
eblaney1 <eblaney1@ivytech.edu>
2024-12-15 14:55:59 -0600

    Add files via upload

---
+++ module 8 functions in python.py
@@ -0,0 +1,15 @@
+# Define the function that compares two numbers
+def greaterThan(x, y):
+    return x > y
+
+# First test case
+a = 2
+b = 3
+result = greaterThan(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+
+# Second test case
+a = 10
+b = 6
+result = greaterThan(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
